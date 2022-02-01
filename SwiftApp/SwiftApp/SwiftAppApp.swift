af1d4eef254b5ce634fcfde0b20cf02c33b140cc
Mihnea Ion <mihnea.ion.ext@siemens.com>
2022-02-01 11:11:50 +0200

    initial setup

---
+++ SwiftApp/SwiftApp/SwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftAppApp.swift
+//  SwiftApp
+//
+//  Created by Mihnea Ion on 01.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
